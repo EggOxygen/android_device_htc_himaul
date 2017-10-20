938621346856eb60b7e6384d3893654c4b569caa
xangma <xangma@gmail.com>
2017-10-20 14:21:46 +0100

    Oreo bringup?
    No idea if this will work …

--- lineage.mk
+++ lineage.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/htc/himaul/full_himaul.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common.mk)
 
 PRODUCT_NAME := lineage_himaul
 BOARD_VENDOR := htc
@@ -10,6 +10,4 @@ PRODUCT_DEVICE := himaul
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE="htc_himaulatt" \
-    PRODUCT_NAME="himaulatt_na_gen_unlock" \
-    BUILD_FINGERPRINT="htc/himaulatt_na_gen_unlock/htc_himaulatt:6.0/MRA58K/669696.12:user/release-keys" \
-    PRIVATE_BUILD_DESC="3.35.617.12 CL669696 release-keys"
+    PRODUCT_NAME="himaulatt_na_gen_unlock" 
